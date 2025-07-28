b6621cc9e21591e9013678c87327c374f9780794
milindpawar007 <milindpawar5555@gmail.com>
2025-07-28 13:59:15 -0500

    sidebar header basic layout is added

---
+++ src/ui/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const StylesHeader = styled.header`
+    
+    background-color: var(--color-grey-0);;
+    padding: 1.2rem 4.8rem;
+    border-bottom:  1px solid var(--color-grey-100);
+`
+function Header() {
+    return (
+        <StylesHeader>
+            Header
+        </StylesHeader>
+    )
+}
+
+export default Header
