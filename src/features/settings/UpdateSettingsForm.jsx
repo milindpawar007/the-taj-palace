4d7e91c7d92cd8b565182500bcea11e1cb6f5c41
milindpawar007 <milindpawar5555@gmail.com>
2025-07-31 19:22:55 -0500

    loading settting data

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,21 +1,27 @@
 import Form from '../../ui/Form';
 import FormRow from '../../ui/FormRow';
 import Input from '../../ui/Input';
+import { useSetting } from './useSetting';
+import Spinner from '../../ui/Spinner'
 
 function UpdateSettingsForm() {
+  const { isLoading, settings: { minBookingLength, maxBookingLength, maxGuestPerBooking, breakfastPrice } = {} } = useSetting();
+
+  if (isLoading) return <Spinner />
   return (
     <Form>
       <FormRow label='Minimum nights/booking'>
-        <Input type='number' id='min-nights' />
+        <Input type='number' defaultValue={minBookingLength} id='min-nights' />
       </FormRow>
       <FormRow label='Maximum nights/booking'>
-        <Input type='number' id='max-nights' />
+        <Input type='number' defaultValue={maxBookingLength} id='max-nights' />
       </FormRow>
       <FormRow label='Maximum guests/booking'>
-        <Input type='number' id='max-guests' />
+        <Input type='number' defaultValue={maxGuestPerBooking} id='max-guests' />
       </FormRow>
       <FormRow label='Breakfast price'>
-        <Input type='number' id='breakfast-price' />
+
+        <Input type='number' defaultValue={breakfastPrice} id='breakfast-price' />
       </FormRow>
     </Form>
   );
