a81d1d53dc5e4a35837a73ff4069c440e19abe79
milindpawar007 <milindpawar5555@gmail.com>
2025-08-01 10:09:59 -0500

    disable button logic is added on updatesetting form

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -18,17 +18,16 @@ function UpdateSettingsForm() {
   return (
     <Form>
       <FormRow label='Minimum nights/booking'>
-        <Input type='number' defaultValue={minBookingLength} id='min-nights' onBlur={(e) => { handelEdit(e, "minBookingLength") }} />
+        <Input type='number' defaultValue={minBookingLength} id='min-nights' disabled={isEditing} onBlur={(e) => { handelEdit(e, "minBookingLength") }} />
       </FormRow>
       <FormRow label='Maximum nights/booking'>
-        <Input type='number' defaultValue={maxBookingLength} id='max-nights' onBlur={(e) => { handelEdit(e, "maxBookingLength") }} />
+        <Input type='number' defaultValue={maxBookingLength} id='max-nights' disabled={isEditing} onBlur={(e) => { handelEdit(e, "maxBookingLength") }} />
       </FormRow>
       <FormRow label='Maximum guests/booking'>
-        <Input type='number' defaultValue={maxGuestPerBooking} id='max-guests' onBlur={(e) => { handelEdit(e, "maxGuestPerBooking") }} />
+        <Input type='number' defaultValue={maxGuestPerBooking} id='max-guests' disabled={isEditing} onBlur={(e) => { handelEdit(e, "maxGuestPerBooking") }} />
       </FormRow>
       <FormRow label='Breakfast price'>
-
-        <Input type='number' defaultValue={breakfastPrice} id='breakfast-price' onBlur={(e) => { handelEdit(e, "breakfastPrice") }} />
+        <Input type='number' defaultValue={breakfastPrice} id='breakfast-price' disabled={isEditing} onBlur={(e) => { handelEdit(e, "breakfastPrice") }} />
       </FormRow>
     </Form>
   );
