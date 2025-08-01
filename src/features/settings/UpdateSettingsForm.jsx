42e04c61073ee91904092967adda558f4ebb6dc6
milindpawar007 <milindpawar5555@gmail.com>
2025-08-01 10:07:27 -0500

    update booking feature is added

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -3,25 +3,32 @@ import FormRow from '../../ui/FormRow';
 import Input from '../../ui/Input';
 import { useSetting } from './useSetting';
 import Spinner from '../../ui/Spinner'
+import useEditSetting from './useEditSetting';
 
 function UpdateSettingsForm() {
   const { isLoading, settings: { minBookingLength, maxBookingLength, maxGuestPerBooking, breakfastPrice } = {} } = useSetting();
+  const { editSetting, isEditing } = useEditSetting();
+  const handelEdit = (e, field) => {
+    const { value } = e.target;
+    if (!value) return;
 
+    editSetting({ [field]: value })
+  }
   if (isLoading) return <Spinner />
   return (
     <Form>
       <FormRow label='Minimum nights/booking'>
-        <Input type='number' defaultValue={minBookingLength} id='min-nights' />
+        <Input type='number' defaultValue={minBookingLength} id='min-nights' onBlur={(e) => { handelEdit(e, "minBookingLength") }} />
       </FormRow>
       <FormRow label='Maximum nights/booking'>
-        <Input type='number' defaultValue={maxBookingLength} id='max-nights' />
+        <Input type='number' defaultValue={maxBookingLength} id='max-nights' onBlur={(e) => { handelEdit(e, "maxBookingLength") }} />
       </FormRow>
       <FormRow label='Maximum guests/booking'>
-        <Input type='number' defaultValue={maxGuestPerBooking} id='max-guests' />
+        <Input type='number' defaultValue={maxGuestPerBooking} id='max-guests' onBlur={(e) => { handelEdit(e, "maxGuestPerBooking") }} />
       </FormRow>
       <FormRow label='Breakfast price'>
 
-        <Input type='number' defaultValue={breakfastPrice} id='breakfast-price' />
+        <Input type='number' defaultValue={breakfastPrice} id='breakfast-price' onBlur={(e) => { handelEdit(e, "breakfastPrice") }} />
       </FormRow>
     </Form>
   );
