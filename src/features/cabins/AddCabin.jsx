ed634c449e979c9979fc964d4103aa427f932106
milindpawar007 <milindpawar5555@gmail.com>
2025-08-07 11:45:37 -0500

    refactor AddCabin and Modal components to support multiple modals and improve structure

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -3,17 +3,43 @@ import Button from '../../ui/Button'
 import CreateCabinForm from './CreateCabinForm'
 import { getCabins } from '../../services/apiCabins'
 import Modal from '../../ui/Modal'
+import CabinTable from './CabinTable'
+
 
 function AddCabin() {
-    const [isopenModal, setIsopenModal] = useState(false)
-    useEffect(() => { getCabins().then((data) => console.log(data)) }, [])
     return (
-        <div>
-            <Button variation="primary" onClick={() => { setIsopenModal(show => !show) }}> Add new Cabin</Button>
+        <Modal>
+            <Modal.Open opens='cabin-form'>
+                <Button variation="primary"> Add new Cabin</Button>
+            </Modal.Open>
+            <Modal.Window name='cabin-form'>
+                <CreateCabinForm />
+            </Modal.Window>
 
-            {isopenModal && <Modal onClose={() => { setIsopenModal(false) }} ><CreateCabinForm onCloseModal={() => { setIsopenModal(false) }} /></Modal>}
-        </div>
+            <Modal.Open opens='table'>
+                <Button variation="primary"> Show Table</Button>
+            </Modal.Open>
+            <Modal.Window name='table'>
+                <CabinTable />
+            </Modal.Window>
+        </Modal>
+
+
     )
 }
+
+
+
+// function AddCabin() {
+//     const [isopenModal, setIsopenModal] = useState(false)
+//     useEffect(() => { getCabins().then((data) => console.log(data)) }, [])
+//     return (
+//         <div>
+//             <Button variation="primary" onClick={() => { setIsopenModal(show => !show) }}> Add new Cabin</Button>
+
+//             {isopenModal && <Modal onClose={() => { setIsopenModal(false) }} ><CreateCabinForm onCloseModal={() => { setIsopenModal(false) }} /></Modal>}
+//         </div>
+//     )
+// }
 
 export default AddCabin
--- src/ui/Modal copy.jsx
+++ src/ui/Modal copy.jsx
@@ -1,5 +1,4 @@
 import PropTypes from "prop-types";
-import { cloneElement, createContext, useContext, useState } from "react";
 import { createPortal } from "react-dom";
 import { HiXMark } from "react-icons/hi2";
 import styled from "styled-components";
@@ -56,54 +55,21 @@ const Button = styled.button`
   }
 `;
 
-const ModalContext = createContext();
 
-function Modal({ children }) {
-  const [openName, setOpenName] = useState('');
-
-  const close = () => setOpenName("");
-  const open = setOpenName;
-  return (
-    <ModalContext.Provider value={{ openName, close, open }}>
-      {children}
-    </ ModalContext.Provider >)
-}
-
-
-function Open({ children, opens: opensWindowName }) {
-
-  const { open } = useContext(ModalContext);
-
-  return cloneElement(children, { onClick: () => open(opensWindowName) });
-
-}
-
-function Window({ children, name }) {
-
-  const { openName, close } = useContext(ModalContext)
-  if (name !== openName) return null;
-
+function Modal({ children, onClose }) {
   return createPortal(
     <Overlay>
       <StyledModal>
-        <Button onClick={close}><HiXMark /></Button>
-        <div>{cloneElement(children, { onCloseModal: close })}</div>
+        <Button onClick={onClose}><HiXMark /></Button>
+        <div>{children}</div>
       </StyledModal>
     </Overlay>,
     document.body
   );
 }
 
-Window.propTypes = {
-  children: PropTypes.element.isRequired,
-  name: PropTypes.string.isRequired,
-};
-
-Modal.Open = Open;
-Modal.Window = Window;
-
 Modal.propTypes = {
-  children: null,
-
+  children: PropTypes.node,
+  onClose: PropTypes.func.isRequired
 };
 export default Modal
