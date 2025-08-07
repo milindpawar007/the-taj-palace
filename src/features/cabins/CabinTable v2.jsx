fd41cd3a948c0f7a36b1afedbe200d9efdc47bea
milindpawar007 <milindpawar5555@gmail.com>
2025-08-07 16:55:49 -0500

    creating menu compoenents

--- src/features/cabins/CabinTable v2.jsx
+++ src/features/cabins/CabinTable v2.jsx
@@ -1,5 +1,13 @@
 import styled from "styled-components";
-import Spinner from '../../ui/Spinner'
+import Spinner from '../../ui/Spinner.jsx'
+const Table = styled.div`
+  border: 1px solid var(--color-grey-200);
+
+  font-size: 1.4rem;
+  background-color: var(--color-grey-0);
+  border-radius: 7px;
+  overflow: hidden;
+`;
 
 const TableHeader = styled.header`
   display: grid;
@@ -19,11 +27,9 @@ const TableHeader = styled.header`
 
 import React from 'react'
 import { useQuery } from "@tanstack/react-query";
-import { getCabins } from "../../services/apiCabins";
-import CabinRow from "./CabinRow";
+import { getCabins } from "../../services/apiCabins.js";
+import CabinRow from "./CabinRow.jsx";
 import useCabins from "../../hooks/useCabins.js";
-import Table from "../../ui/Table.jsx";
-import Menus from "../../ui/Menus.jsx";
 
 function CabinTable() {
 
@@ -34,25 +40,17 @@ function CabinTable() {
   const { isLoading, cabins, error } = useCabins();
   if (isLoading) return <Spinner />
   return (
-    <Menus>
-      <Table columns='0.6fr 1.8fr 2.2fr 1fr 1fr 1fr'>
-        <Table.Header>
-          <div></div>
-          <div>Cabin</div>
-          <div>Capacity</div>
-          <div>Price</div>
-          <div>Discount</div>
-          <div></div>
-        </Table.Header>
-        <Table.Body
-          data={cabins}
-          render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
-        />
-
-
-
-      </Table>
-    </Menus>
+    <Table role='table'>
+      <TableHeader role='row'>
+        <div></div>
+        <div>Cabin</div>
+        <div>Capacity</div>
+        <div>Price</div>
+        <div>Discount</div>
+        <div></div>
+      </TableHeader>
+      {cabins?.map((cabin) => <CabinRow cabin={cabin} key={cabin.id} />)}
+    </Table>
   )
 }
 
--- src/ui/Table.jsx
+++ src/ui/Table.jsx
@@ -1,4 +1,6 @@
+import { createContext, useContext } from "react";
 import styled from "styled-components";
+import PropTypes from "prop-types";
 
 const StyledTable = styled.div`
   border: 1px solid var(--color-grey-200);
@@ -58,3 +60,62 @@ const Empty = styled.p`
   text-align: center;
   margin: 2.4rem;
 `;
+
+const TableContext = createContext()
+function Table({ columns, children }) {
+
+  return (
+
+    <TableContext.Provider value={{ columns }}>
+      <StyledTable role="table">
+        {children}
+      </StyledTable>
+    </TableContext.Provider>
+  )
+
+
+}
+function Header({ children }) {
+
+  const { columns } = useContext(TableContext);
+
+  return (
+    <StyledHeader role="row" columns={columns} as="Header">{children}</StyledHeader>
+  )
+}
+function Row({ children }) {
+  const { columns } = useContext(TableContext);
+
+  return (
+    <StyledRow role="row" columns={columns}>{children}</StyledRow>
+  )
+}
+function Body({ data, render }) {
+
+  if (!data.length) return <Empty>No Data to show at the moment</Empty>
+  return (
+    <StyledBody>
+      {data.map(render)}
+    </StyledBody>
+  )
+}
+Table.Header = Header;
+Table.Row = Row;
+Table.Body = Body;
+Table.Footer = Footer;
+
+Table.propTypes = {
+  columns: PropTypes.string,
+  children: PropTypes.node.isRequired,
+};
+Header.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+Row.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+Body.propTypes = {
+  data: PropTypes.array.isRequired,
+  render: PropTypes.func.isRequired,
+}
+export default Table
