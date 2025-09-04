77ed9e06793d480d40bd9175b72deca49a6f4f21
milindpawar007 <milindpawar5555@gmail.com>
2025-09-04 17:39:20 -0500

    implement authentication flow with login and protected routes; add loading states and error handling

---
+++ src/features/authentication/useGetAuthUser.js
@@ -0,0 +1,13 @@
+import {  useQuery } from "@tanstack/react-query";
+
+import { getCurrentUser }  from "../../services/apiAuth";
+
+
+export function useGetAuthUser(){
+
+  const {isLoading,data:user} =useQuery({
+     queryKey:["user"],
+     queryFn:getCurrentUser
+  });
+   return {user,isLoading,isAuthenticated : user?.role==="authenticated"}
+}
